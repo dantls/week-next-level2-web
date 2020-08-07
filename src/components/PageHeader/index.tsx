9b8c765808540fd7b0c80c7e4f4db3926c609789
dantls <dan_tls@hotmail.com>
2020-08-06 23:42:04 -0400

    Lesson 1 finished

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
 
+import './styles.css';
+
 interface PageHeaderProps {
   title: string;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({title}) => {
+const PageHeader: React.FC<PageHeaderProps> = ({children,title}) => {
   return (
     <header className="page-header">
       <div className="top-bar-container">
@@ -19,6 +21,7 @@ const PageHeader: React.FC<PageHeaderProps> = ({title}) => {
       </div>
       <div className="header-content">
         <strong>{title}</strong>
+        {children}
       </div>
     </header>
   )
---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,41 @@
+
+import React from 'react';
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import './styles.css';
+
+const TeacherItem: React.FC = () => {
+  return (
+    <article className="teacher-item">
+            <header>
+              <img
+                src="https://media-exp1.licdn.com/dms/image/C4E03AQEcdtKH1XEaCg/profile-displayphoto-shrink_100_100/0?e=1602115200&v=beta&t=ATuPiAOWqU7Tr8w6M4nYhypkeDigv0ZkeeKOfEfAcOI"
+                alt="Danilo Gomes"
+              />
+              <div>
+                <strong>Danilo Gomes</strong>
+                <span>Geografia</span>
+              </div>
+            </header>
+            <p>
+              Entusiasta das melhores tecnologias em geografia avançada.
+              <br/><br/>
+              Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas.
+            </p>
+
+            <footer>
+            <p>Preço/hora
+              <strong>R$ 40,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Whatsapp"/>
+              Entrar em contato
+            </button>
+
+          </footer>
+          </article>
+  )
+}
+
+export default TeacherItem;
+
+
